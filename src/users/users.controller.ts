da28fcf89d85949bb0963be310206006069a94d3
bassem eloraby <BASEM12683@GMAIL.COM>
2023-04-28 08:43:51 +0300

    parse uuid

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Body,
   HttpCode,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -15,15 +16,16 @@ import { v4 as uuid } from 'uuid';
 
 @Controller('users')
 export class UsersController {
-  private readonly users: UserEntity[] = [];
+  private users: UserEntity[] = [];
   @Get()
   find(): UserEntity[] {
     return this.users;
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string): UserEntity {
-    return this.users.find((user) => user.id === id);
+  findOne(@Param('id', ParseUUIDPipe) id: string): UserEntity {
+    console.log(typeof id);
+    return this.users.find((user: UserEntity) => user.id === id);
   }
 
   @Post()
@@ -38,13 +40,22 @@ export class UsersController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return updateUserDto;
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    // find the index of element we update
+    const index = this.users.findIndex((user: UserEntity) => user.id === id);
+    // update element
+    this.users[index] = { ...this.users[index], ...updateUserDto };
+    return this.users[index];
   }
 
   @Delete(':id')
   @HttpCode(204)
-  remove(@Param('id') id: any): string {
-    return id;
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    // find the index of element we delete
+    this.users = this.users.filter((user) => user.id !== id);
+    // delete the element
   }
 }
